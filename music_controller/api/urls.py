838739f63e9736a3f51b63ef48b7895f7ee612d0
Ethan_Williams <etwilliams28@gmail.com>
2021-06-18 18:00:34 -0700

    Update view.py:  create class - UserInRoom apiview
     Purpose:
     logic for redirecting existing users in a room at home page to room url
    
     Update HomePage.js:
     add frontend component for homepage view
     add logic for redirect and templating for UserInRoom Class

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import RoomView
-from .views import CreateRoomView,GetRoom,JoinRoom
+from .views import CreateRoomView,GetRoom,JoinRoom,UserInRoom
 
 
 urlpatterns = [
     path('', RoomView.as_view()),
     path('create-room', CreateRoomView.as_view()),
     path('get-room', GetRoom.as_view()),
-    path('join-room', JoinRoom.as_view())
+    path('join-room', JoinRoom.as_view()),
+    path('user-in-room', UserInRoom.as_view())
 ]
